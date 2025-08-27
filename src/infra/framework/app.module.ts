50eca142edd31730cb298a8eba456857f7dfa26b
Leandro Liduvino <leandro.augusto460@gmail.com>
2025-08-27 19:03:19 -0300

    feat: add typeorm to project

--- src/infra/framework/app.module.ts
+++ src/infra/framework/app.module.ts
@@ -1,7 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/domain/entities/user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: 5555,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [User],
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
