a7420d4eb123c357d2471cebe3cd883f65025adc
Leandro Liduvino <leandro.augusto460@gmail.com>
2025-08-27 22:28:11 -0300

    feat: enhance GetTaskController with ApiOkResponse and ApiNotFoundResponse for improved API documentation

--- src/infra/controllers/tasks/get-task/get-task.controller.ts
+++ src/infra/controllers/tasks/get-task/get-task.controller.ts
@@ -1,5 +1,10 @@
 import { Controller, Get, Param, UseGuards } from '@nestjs/common';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { GetTaskDto } from 'src/application/tasks/get-task/get-task.dto';
 import { GetTaskUseCase } from 'src/application/tasks/get-task/get-task.usecase';
 import { AuthUser } from 'src/infra/decorators/auth-user.decorator';
@@ -16,6 +21,22 @@ export class GetTaskController {
   constructor(private readonly getTaskUseCase: GetTaskUseCase) {}
 
   @Get(':id')
+  @ApiOkResponse({
+    description: 'Task retrieved successfully',
+    schema: {
+      example: {
+        id: '1',
+        title: 'Task Title',
+        description: 'Task Description',
+        userId: '1',
+        createdAt: '2023-10-10T10:00:00.000Z',
+      },
+    },
+  })
+  @ApiNotFoundResponse({
+    description: 'Task not found',
+    schema: { example: { statusCode: 404, message: 'Task not found' } },
+  })
   async execute(@Param() param: GetTaskDto, @AuthUser() user: AuthUserDto) {
     return this.getTaskUseCase.execute({
       ...param,
