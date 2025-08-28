90a67b41f95592bead7d5af4b570bd6bbd06fca5
Leandro Liduvino <leandro.augusto460@gmail.com>
2025-08-27 21:24:20 -0300

    feat: enhance GetTaskDto with ApiProperty for better Swagger documentation

--- src/application/tasks/get-task/get-task.dto.ts
+++ src/application/tasks/get-task/get-task.dto.ts
@@ -1,6 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsUUID } from 'class-validator';
 
 export class GetTaskDto {
+  @ApiProperty({
+    example: 'a1b2c3d4-e5f6-7g8h-9i0j-k1l2m3n4o5p6',
+    description: 'Task ID',
+  })
   @IsString()
   @IsUUID()
   id: string;
--- src/infra/controllers/tasks/get-task/get-task.controller.ts
+++ src/infra/controllers/tasks/get-task/get-task.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, Param, UseGuards } from '@nestjs/common';
-import { ApiBearerAuth, ApiParam, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { GetTaskDto } from 'src/application/tasks/get-task/get-task.dto';
 import { GetTaskUseCase } from 'src/application/tasks/get-task/get-task.usecase';
 import { AuthUser } from 'src/infra/decorators/auth-user.decorator';
@@ -16,11 +16,6 @@ export class GetTaskController {
   constructor(private readonly getTaskUseCase: GetTaskUseCase) {}
 
   @Get(':id')
-  @ApiParam({
-    name: 'id',
-    description: 'Task ID',
-    example: 'a1b2c3d4-e5f6-7g8h-9i0j-k1l2m3n4o5p6',
-  })
   async execute(@Param() param: GetTaskDto, @AuthUser() user: AuthUserDto) {
     return this.getTaskUseCase.execute({
       ...param,
